190908c8645d2a9e2d45a27d6e66ee24ea94bee2
sureshhewa <sureshhewabi@gmail.com>
2024-11-20 10:06:01 +0000

    CICD changes

--- scripts/.runStat.sh
+++ scripts/.runStat.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 ##### VARIABLES
-ROOT_DIR=$ARCHIVE_ROOT_DIR
 RUN_NAME="File-Download-Stat-$(date +"%Y")-$(date +"%b")-$((RANDOM % 9000 + 1000))"
 LOG_FILE="${LOG_FOLDER}${RUN_NAME}_nextflow.log"
+PROFILE=$1
 
 ${NEXTFLOW}nextflow -log $LOG_FILE run ${PIPELINE_BASE_DIR}main.nf \
               -name $RUN_NAME \
+              -profile $PROFILE \
               --nextflow_location $NEXTFLOW \
               --root_dir $DATA_ROOT_DIR \
               --log_file $LOG_FILE
