627b8bc688ca8d5dd390fd1f368877f7fb70d656
sureshhewa <sureshhewabi@gmail.com>
2024-12-06 14:58:55 +0000

    add violin plot by protocol

--- filedownloadstat/log_file_stat.py
+++ filedownloadstat/log_file_stat.py
@@ -13,8 +13,33 @@ class LogFileStat:
         data = pd.read_csv(file, sep="\t", header=None, names=["file_path", "filename", "size", "lines"])
 
         # Create histogram of file sizes
-        fig = px.histogram(data, x="size", title="File Size Distribution", labels={"size": "File Size (Bytes)"})
+        fig = px.histogram(data, x="size", title="File Size Distribution", labels={"size": "File Size"})
         fig.write_html("file_size_distribution.html")
+
+    @staticmethod
+    def plot_violin_for_protocols(file_list: str):
+        """
+        Create violin plots of file size distributions for each protocol without relying on column headers.
+        """
+        # Load data without headers
+        data = pd.read_csv(file_list, sep="\t", header=None)
+
+        # Assign meaningful column names
+        data.columns = ["path", "filename", "size", "protocol"]
+
+        # Create violin plot grouped by protocol
+        fig = px.violin(
+            data,
+            y="size",
+            x="protocol",
+            color="protocol",
+            box=True,  # Add a box plot inside the violin
+            points="outliers",  # Show all data points
+            title="File Size Distribution by Protocol",
+            labels={"size": "File Size (Bytes)", "protocol": "Protocol Type"},
+        )
+        fig.write_html("file_size_violin_by_protocol.html")
+        print("Violin plot written to file_size_violin_by_protocol.html")
 
     # @staticmethod
     # def number_of_lines_vs_file_size(file):
@@ -38,7 +63,7 @@ class LogFileStat:
         """
         # Generate the visualizations
         LogFileStat.log_file_size_distribution(file)
-        # LogFileStat.number_of_lines_vs_file_size(file)
+        LogFileStat.plot_violin_for_protocols(file)
 
         # Combine the HTML files
         with open(output, "w") as f:
@@ -47,5 +72,5 @@ class LogFileStat:
             # Embed the content of the generated HTML plots
             with open("file_size_distribution.html", "r") as dist_file:
                 f.write(dist_file.read())
-            # with open("number_of_lines_vs_file_size.html", "r") as lines_file:
-            #     f.write(lines_file.read())
+            with open("file_size_violin_by_protocol.html", "r") as lines_file:
+                f.write(lines_file.read())
