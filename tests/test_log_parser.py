4e95fd9b046be490576bf39862bc6c7a87741dde
sureshhewa <sureshhewabi@gmail.com>
2025-01-31 10:34:44 +0000

    Test case for log parsing -  globus record

--- tests/test_log_parser.py
+++ tests/test_log_parser.py
@@ -21,12 +21,20 @@ class TestLogParser(unittest.TestCase):
         )
     def test_valid_row(self):
         """Test parsing a correctly formatted row."""
-        row = [
-            "2023-01-01T23:57:16.000Z", "caf4815df53c650523eb4943a05bc7b7906e5507",
-            "669943668", "/pride/data/archive/2016/12/PXD004242/filename.raw",
-            "OUT", "03dbae9a96db63fa62487cd3c134d05230858127", "Complete",
-            "United Kingdom", "Cambridgeshire", "Sawston", "52.1202,0.1845", "http", "public"
-        ]
+        row = [ "2023-01-01T23:57:16.000Z",
+                "caf4815df53c650523eb4943a05bc7b7906e5507",
+                "669943668",
+                "/pride/data/archive/2016/12/PXD004242/filename.raw",
+                "OUT",
+                "03dbae9a96db63fa62487cd3c134d05230858127",
+                "Complete",
+                "United Kingdom",
+                "Cambridgeshire",
+                "Sawston",
+                "52.1202,0.1845",
+                "http",
+                "public"
+               ]
         parsed = self.parser.parse_row(row, 1)
 
         self.assertIsInstance(parsed, dict)
@@ -59,6 +67,26 @@ class TestLogParser(unittest.TestCase):
         parsed = self.parser.parse_row(row,  3)
         self.assertIsNone(parsed)  # Should return None because the accession doesn't match
 
+    def test_valid_row_globus(self):
+        """Test parsing a correctly formatted row."""
+
+        row = [
+                "2023-11-16T14:17:18.963Z",
+                "206692352bb826652461c90c0e056045282f508d",
+                "619815434",
+                "/xfer/public/pride/data/archive/2023/11/PXD012558/LF_Natowicz_10_091916.raw.mgf",
+                "OUT",
+                "f1e078517ffda4b7cc0389661184ecc05cf6682f",
+                "Complete",
+                "United Kingdom",
+                "Cambridgeshire",
+                "Sawston",
+                "52.1202,0.1845",
+                "gridftp-globus",
+                "public"
+                ]
+        parsed = self.parser.parse_row(row, 1)
+        self.assertIsNotNone(parsed, dict)
 
 if __name__ == '__main__':
     unittest.main()
